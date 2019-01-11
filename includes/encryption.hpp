12588788dace91a7c32434340eb5f13ba5a60b02
AndreevSemen <43631421+AndreevSemen@users.noreply.github.com>
2019-01-11 18:47:22 +0300

    Create encryption.hpp

---
+++ includes/encryption.hpp
@@ -0,0 +1,12 @@
+// Copyright 2019 SemenAndreev
+
+#ifndef ENCRYPTION_HPP
+#define ENCRYPTION_HPP
+
+#include <sstream>
+#include <string>
+#include <modules.hpp>
+
+std::string Encryption(std::string& source, const __uint32_t key);
+
+#endif // ENCRYPTION_HPP
