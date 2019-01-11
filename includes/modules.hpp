2fccddbf363951734ba3d01786dd8045c7eca356
AndreevSemen <43631421+AndreevSemen@users.noreply.github.com>
2019-01-11 18:46:10 +0300

    Create modules.hpp

---
+++ includes/modules.hpp
@@ -0,0 +1,18 @@
+// Copyright 2019 SemenAndreev
+
+#ifndef MODULES_HPP
+#define MODULES_HPP
+
+#include <cmath>
+
+#define ROTATION_INDEX 17
+#define BLOCK_SIZE 32
+#define MODULE static_cast<__uint32_t>(pow(2, BLOCK_SIZE) - 1)
+#define N 32
+
+__uint32_t XOR(const __uint32_t block, const __uint32_t key);
+__uint32_t CircleRotation(const __uint32_t block, const unsigned char rotationIndex);
+__uint32_t ModAdditation(const __uint32_t firstBlock, const __uint32_t secondBlock, const __uint32_t module);
+__uint32_t ModSubtruction(const __uint32_t unknownBlock, const __uint32_t knownBlock, const __uint32_t module);
+
+#endif // MODULES_HPP
