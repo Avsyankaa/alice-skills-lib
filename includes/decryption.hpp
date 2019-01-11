3119c48c3b5de34159b60573f47797a290ef679b
AndreevSemen <43631421+AndreevSemen@users.noreply.github.com>
2019-01-11 18:48:14 +0300

    Create decryption.hpp

---
+++ includes/decryption.hpp
@@ -0,0 +1,12 @@
+// Copyright 2019 SemenAndreev
+
+#ifndef DECRYPTION_HPP
+#define DECRYPTION_HPP
+
+#include <sstream>
+#include <string>
+#include <modules.hpp>
+
+std::string Decryption(std::string& cryptoText, const __uint32_t key);
+
+#endif // DECRYPTION_HPP
